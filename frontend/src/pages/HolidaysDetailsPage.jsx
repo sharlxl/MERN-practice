53d1d5be81dfca2b2366a7bac022e4d4187bba95
sharlxl <sharleneee@gmail.com>
2022-06-15 16:39:58 +0800

    linked to BE to fetch individual holidays

--- frontend/src/pages/HolidaysDetailsPage.jsx
+++ frontend/src/pages/HolidaysDetailsPage.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const HolidaysDetailsPage = () => {
+  const [holiday, setHoliday] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/api/holidays/${id}`)
+      .then((response) => response.json())
+      .then((data) => setHoliday(data));
+  }, []);
+  if (holiday.status === "fail") {
+    return "No Holiday here";
+  }
   return (
-    <>
-      <h1>Holidays Details Page</h1>
-      <h2>Harvey Wallbanger Day</h2>
+    <section>
+      <h1 className="text-3xl">Holidays Details Page</h1>
+      <h2 className="text-2xl">{holiday.name}</h2>
       <dl>
         <dt>Celebrated</dt>
-        <dd>Not Celebrated</dd>
+        <dd className="italic">{holiday.celebrated ? "yes" : "no"}</dd>
         <dt>Likes</dt>
-        <dd>17444</dd>
+        <dd className="font-bold">{holiday.likes}</dd>
         <dt>Description</dt>
-        <dd>Who has seen Old Tom Collins</dd>
+        <dd>{holiday.description}</dd>
       </dl>
-    </>
+    </section>
   );
 };
 
