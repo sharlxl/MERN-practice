3bd1910fbdbeaaf7ff4d9e8e3604283f47f0ca0e
sharlxl <sharleneee@gmail.com>
2022-06-15 16:42:46 +0800

    added a fetch for holiday list

--- frontend/src/pages/HolidaysPage.jsx
+++ frontend/src/pages/HolidaysPage.jsx
@@ -1,52 +1,46 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import CreateHolidayForm from "../components/CreateHolidayForm";
+import HolidaysListCard from "../components/HolidaysListCard";
 import NavBar from "../components/NavBar";
 
 const HolidaysPage = () => {
+  const [holidaysList, setHolidaysList] = useState([]);
+  useEffect(() => {
+    fetch(`/api/holidays/all`)
+      .then((response) => response.json())
+      .then((data) => setHolidaysList(data));
+  }, []);
+
+  if (holidaysList.status === "fail") {
+    return holidaysList.data;
+  }
+
   return (
     <>
       <NavBar />
       <section>
         <h1 className="text-2xl">Holidays</h1>
-        <form>
-          <fieldset>
-            <legend>Add a new Holiday</legend>
-            <label htmlFor="holiday">Name</label>
-            <input
-              id="holiday"
-              name="holiday"
-              type="text"
-              placeholder="New Holiday!"
-            />
-            <button>Add new!</button>
-          </fieldset>
-        </form>
+        <div className="flex justify-center">
+          <CreateHolidayForm />
+        </div>
       </section>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Reason</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>World Kindness Day</td>
-            <td>9999</td>
-            <td>Like</td>
-            <td>Edit</td>
-            <td>Delete</td>
-          </tr>
-          <tr>
-            <td>Bathtub Party Day</td>
-            <td>12583</td>
-            <td>Like</td>
-            <td>Edit</td>
-            <td>Delete</td>
-          </tr>
-        </tbody>
-      </table>
-      <Outlet />
+      <section className="flex gap-4">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Reason</th>
+            </tr>
+          </thead>
+          <tbody>
+            {holidaysList.map((holiday) => {
+              return <HolidaysListCard key={holiday._id} {...holiday} />;
+            })}
+          </tbody>
+        </table>
+        <Outlet />
+      </section>
     </>
   );
 };
